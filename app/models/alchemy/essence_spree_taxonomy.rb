d0f830d513b0f30f865e4ee1f27911a828979fff
Ben A Morgan <ben@benmorgan.io>
2014-06-30 23:50:11 -0700

    fixes issue #4 Closes #5
    remove readonly for taxon
    
    remove readonly for taxonomy

--- app/models/alchemy/essence_spree_taxonomy.rb
+++ app/models/alchemy/essence_spree_taxonomy.rb
@@ -1,6 +1,6 @@
 module Alchemy
   class EssenceSpreeTaxonomy < ActiveRecord::Base
-    belongs_to :taxonomy, class_name: "Spree::Taxonomy", foreign_key: 'taxonomy_id', readonly: true
+    belongs_to :taxonomy, class_name: "Spree::Taxonomy", foreign_key: 'taxonomy_id'
 
     acts_as_essence(
         ingredient_column: 'taxonomy_id',
